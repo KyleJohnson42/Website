a639469ee8808e02c9602ffda79d6111f3f0e9d9
KyleJohnson42 <zion4to6@gmail.com>
2025-07-03 22:48:44 -0700

    Fix email endpoint

--- server/src/functions/sendEmail.js
+++ server/src/functions/sendEmail.js
@@ -6,20 +6,15 @@ app.http('sendEmail', {
     methods: ['POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-        console.log(`Http function processing request for url "${request.url}"`);
-        console.log(request);
-
         const { data, error } = await resend.emails.send({
             from: "kyle@kylejohnsondev.com",
             to: ["kylejohnson@mailfence.com"],
-            subject: `Message from ${request.body.name || '[No name specified]'} through your personal website!`,
-            html: `From ${request.body.email || '[No email specified]'}: ${request.body.message || '[No message specified]'}`
+            subject: `Message from ${request.params.name} through your personal website!`,
+            html: `From ${request.params.email}: ${request.params.message}`
         });
 
-        context.log(`Http function processed request for url "${request.url}"`);
+        context.log(data, error);
 
-        const name = request.query.get('name') || await request.text() || 'world';
-
-        return { body: `Hello, ${name}!` };
+        return { body: JSON.stringify(data) };
     }
 });
